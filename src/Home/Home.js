c30217d11cefe57f1213dada0a2a3489e5786f39
407 <407@academicos.cesde.edu.co>
2022-11-29 16:38:59 -0500

    se crea proyecto

---
+++ src/Home/Home.js
@@ -0,0 +1,78 @@
+import './Home.css'
+export function Home(){
+
+return(
+
+    <>
+    <section>
+        <div class="banner">
+
+        </div>
+    </section>
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-md-8">
+                    <h1>Gorillaz</h1>
+
+                    <p>Gorillaz es una banda británica creada en 1998 por Damon Albarn y Jamie Hewlett. La banda utiliza cuatro personajes ficticios los cuales son: 2-D, Noodle, Murdoc Niccals y Russel Hobbs. Su nombre tiene un motivo, se debe a que sus creadores nacieron, según el calendario chino, en el año del mono.
+
+                    En su inicio vendieron más de 6 millones de copias del álbum “Gorillaz”. Gracias a esto tuvieron una entrada al libro Guinnes de los Récords como la banda virtual más exitosa.
+
+                    Sus siguientes álbumes fueron “Demon Days” en 2005, “The Fall” y “Plastic Beach” en 2010. Con este último álbum también consiguieron un record. Tuvieron más de 900 mil reproducciones en YouTube con su video “Stylo” en las primeras 24 horas.
+
+                    Jamie Hewlett alguna vez mencionó que “si alguien mira MTV durante demasiado tiempo, podrá darse cuenta que es como el infierno, porque no hay nada entretenido allí”. De ahí surgieron las ganas de crear una banda de dibujos animados. En esos tiempos cuando YouTube no existía.
+
+                    Los personajes principales se han usado para portadas de discos, conciertos, carteles y más.
+
+                    Otro producto más de Gorillaz fue su película “Bananaz”. Es un film que cuenta la historia de la asociación y la comunidad detrás de Gorillaz y muestra partes de la banda que anteriormente no se sabían.
+
+                    También la banda cuenta un producto escrito que trata sobre la historia de los personajes Gorillaz. Se escribió el libro “Rise of the Ogre” (El ascenso del ogro) para relatar la vida ficticia de los integrantes. Un elemento necesario para dar coherencia a un concepto de animación.</p>
+
+                </div>
+
+                
+
+            
+                <div class="col-12 col-md-4"></div>
+                <i class="bi bi-vinyl-fill fs-1 "></i>
+                <i class="bi bi-vinyl-fill fs-1 "></i>
+                <i class="bi bi-vinyl-fill fs-1 "></i>
+                <i class="bi bi-vinyl-fill fs-1 "></i>
+                <i class="bi bi-vinyl-fill fs-1 "></i>
+
+                
+        
+
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12 col-md-6">
+
+                    <img src="https://firebasestorage.googleapis.com/v0/b/spotifyjdg-eff06.appspot.com/o/6183d6eeb812f.jpg?alt=media&token=0ac417c7-e3ac-44a8-b12c-e6d770f36880" alt="Concietro Gorillaz en Bogota" class="img-fluid w-100"/>
+                </div>
+
+                <div class="col-12 col-md-6">
+
+                    <img src="https://firebasestorage.googleapis.com/v0/b/spotifyjdg-eff06.appspot.com/o/6183d6eeb812f.jpg?alt=media&token=0ac417c7-e3ac-44a8-b12c-e6d770f36880" alt="Concietro Gorillaz en Bogota" class="img-fluid w-100"/>
+
+                </div>
+
+                <div class="col-12 col-md-6">
+
+                    <img src="https://firebasestorage.googleapis.com/v0/b/spotifyjdg-eff06.appspot.com/o/6183d6eeb812f.jpg?alt=media&token=0ac417c7-e3ac-44a8-b12c-e6d770f36880" alt="Concietro Gorillaz en Bogota" class="img-fluid w-100"/>
+                </div>
+
+            </div>
+        </div>
+        
+    </section>
+    </>
+
+
+)
+
+}
